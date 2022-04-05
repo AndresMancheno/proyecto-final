39e8f92f2f6835e13ff35ffd5b47c104b254394b
Fernando Martinez Lopez <fernandomartinez.alu@iespacomolla.es>
2022-04-05 19:25:53 +0200

    Footer y estilo con codigo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
+import Footer from '../components/Footer/footer';
 import { Layout } from '../components/Layout/Layout';
 
 export default function Home() {
 	return (
 		<Layout>
 			<h1>Home</h1>
+			<Footer />
 		</Layout>
 	);
 }
